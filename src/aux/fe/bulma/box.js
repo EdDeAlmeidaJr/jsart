069518e141efd682b3c4f57cd34af5ceb8376858
Ed de Almeida <edvaldoajunior@gmail.com>
2024-12-27 16:51:03 -0300

    Box test

--- src/aux/fe/bulma/box.js
+++ src/aux/fe/bulma/box.js
@@ -1,13 +1,15 @@
+const consoleOutput = require('../../dev/consoleoutput');
+
 const bulmaBox = (elem, htmlContent) => {
   if (!elem) {
     elem = "div"
   }
   if (!htmlContent) {
-    console.error('**bulmaBox** => the \'htmlContent\' parameter must be non-null.');
+    consoleOutput('error', null, '**bulmaBox** => the \'htmlContent\' parameter must be non-null.');
     return null;
   }
   let bx = document.createElement(elem);
-  bx.setAttribute('class','box');
+  bx.setAttribute('class', 'box');
   bx.appendChild(htmlContent);
   return bx;
 }
--- src/aux/fe/bulma/box.test.js
+++ src/aux/fe/bulma/box.test.js
@@ -1,5 +1,9 @@
+const consoleOutput = require('../../dev/consoleoutput');
+
 const bulmaBox = require('./box');
 
 if (process.env.__TEST__) {
-  
+
+  (bulmaBox(null, null) === null) ? consoleOutput('log', 'green', 'Ok!') : consoleOutput('error', null, 'Failed...');
+
 }
