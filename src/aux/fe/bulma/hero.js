6533e272ce92ac48ca7828f54683478db6da60fb
Ed de Almeida <edvaldoajunior@gmail.com>
2024-12-27 17:51:37 -0300

    Tests for Hero

--- src/aux/fe/bulma/hero.js
+++ src/aux/fe/bulma/hero.js
@@ -1,4 +1,18 @@
+const consoleOutput = require('../../dev/consoleoutput');
+
 const bulmaHero = (classesList, htmlContent) => {
+  if (!classesList) {
+    consoleOutput('error', null, '**bulmaHero** => the parameter \'classesList\' nust be non-null;');
+    return null;
+  }
+  if (typeof classesList !== 'Array') {
+    consoleOutput('error', null, '**bulmaHero** => the parameter \'classesList\' must be of type array.');
+    return null;
+  }
+  if (!htmlContent) {
+    consoleOutput('error', null, '**bulmaHero** => the parameter \'htmlContent\' nust be non-null;');
+    return null;
+  }
   const hero = document.createElement('section');
   hero.setAttribute('class', 'hero');
   classesList.map(cl => {
--- src/aux/fe/bulma/hero.test.js
+++ src/aux/fe/bulma/hero.test.js
@@ -1,5 +1,11 @@
+const consoleOutput = require('../../dev/consoleoutput');
 const bulmaHero = require('./hero');
 
 if (process.env.__TEST__) {
 
+  (bulmaHero(null, null) === null) ? consoleOutput('log', 'green', 'Ok!') : consoleOutput('error', null, 'Failed...');
+
+  (bulmaHero('teste', null) === null) ? consoleOutput('log', 'green', 'Ok!') : consoleOutput('error', null, 'Failed...');
+
+  (bulmaHero([], null) === null) ? consoleOutput('log', 'green', 'Ok!') : consoleOutput('error', null, 'Failed...');
 }
